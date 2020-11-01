625b1b0d1615844e67a3d4adeb90921d97eb5e69
namnguyen1996 <namnguyen@innoria.com>
2020-11-01 12:40:50 +0700

    fix import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NavigationComponent } from './navigation/navigation.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { FooterComponent } from './footer/footer.component';
 import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
-import { UtterancesDirective } from './utterances.directive';
 
 @NgModule({
   declarations: [
@@ -15,7 +13,6 @@ import { UtterancesDirective } from './utterances.directive';
     NavigationComponent,
     FooterComponent,
     ScrollToTopComponent,
-    UtterancesDirective
   ],
   imports: [
     BrowserModule,
