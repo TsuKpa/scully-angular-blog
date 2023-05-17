352d6a35a5a6541b5fd1ba1a996f9b8219e78df1
tsukpa <tsunamikampa@gmail.com>
2023-05-17 21:08:38 +0700

    fix error title tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SocialTagsService } from './social-tags.service';
+import { Title } from "@angular/platform-browser";
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    public constructor(private tagsService: SocialTagsService) {
+export class AppComponent implements OnInit {
+    public constructor(private tagsService: SocialTagsService,
+        private titleService: Title) {
         this.tagsService.setTitleAndTags();
+    }
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.titleService.setTitle('TsuKpa Blog');
+        }, 100);
     }
 }
