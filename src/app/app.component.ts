ac08fda2c243d9d834dd044135173799e17afc2e
tsukpa <tsunamikampa@gmail.com>
2023-08-13 08:41:18 +0700

    fix title tag remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         setTimeout(() => {
             this.titleService.setTitle('TsuKpa Blog - Nam Quang Nguyen');
-        }, 110);
+        }, 150);
         this.toggleDarkThemeService.isToggle$.subscribe(toggle => {
             if (toggle) {
                 this.renderer.addClass(document.body, 'dark-theme');
