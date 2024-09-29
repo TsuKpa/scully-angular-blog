6672d08e7c34787f10887a7d7354f2666123372e
tsukpa <tsunamikampa@gmail.com>
2024-09-29 19:59:31 +0700

    update css workshop

--- src/app/aws-workshops/aws-workshops-data.ts
+++ src/app/aws-workshops/aws-workshops-data.ts
@@ -12,7 +12,7 @@ export const workshops: WorkShop[] = [
         id: 1,
         title: "Deploy Static Site NextJS App to S3 using Github Action",
         description: "In this workshop, you’ll learn the basics and practice of Amazon S3, Cloudfront, using IAM OIDC STS with Github Action. Perform creating NextJs App with low latency.        ",
-        url: "https://d9akteslg4v3w.cloudfront.net/001-deploy-static-site-to-s3/",
+        url: "https://ws.tsukpa.blog/001-deploy-static-site-to-s3/",
         techStacks: ["S3", "IAM", "Cloudfront", "Github Actions"],
         isHaveTerraform: true
     }
